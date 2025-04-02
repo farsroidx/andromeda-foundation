811fe409e59cf9cb75e21f1383c3e19339adf83f
Mohammad ali Riazati <admin@farsroidx.ir>
2025-04-02 04:49:48 +0330

    Initial commit

--- m31/build.gradle.kts
+++ m31/build.gradle.kts
@@ -36,7 +36,7 @@ android {
     }
 
     buildFeatures {
-        buildConfig = true
+        buildConfig = false
     }
 
     compileOptions {
@@ -73,7 +73,7 @@ publishing {
 
             groupId    = "ir.farsroidx.m31"
             artifactId = "andromeda-foundation"
-            version    = "2.0.0"
+            version    = "2.0.1"
 
             afterEvaluate {
 
